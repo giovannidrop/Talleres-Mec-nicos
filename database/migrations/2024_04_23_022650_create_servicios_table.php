0e18e492771ae1eb71e92689214fedf40e16258e
Giovanni Saavedra <giovannisaavedra160@gmail.com>
2024-04-22 21:32:07 -0500

    migracion

---
+++ database/migrations/2024_04_23_022650_create_servicios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servicios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vehiculo_id');
+            $table->foreign('vehiculo_id')->references('id')->on('vehiculos')->onDelete('cascade');
+            $table->date('fecha_servicio');
+            $table->text('descripcion');
+            $table->decimal('costo', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servicios');
+    }
+};
