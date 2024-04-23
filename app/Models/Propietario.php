2723ead2268697e5c180882e6afe4ebadc283a81
Giovanni Saavedra <giovannisaavedra160@gmail.com>
2024-04-23 09:53:00 -0500

    ajuste de la vista

---
+++ app/Models/Propietario.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Propietario extends Model
+{
+    use HasFactory;
+
+    protected $table = 'propietarios';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+}
