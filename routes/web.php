de2fd1d066a7c18cab94c653252b6f92fcbab06d
Giovanni Saavedra <giovannisaavedra160@gmail.com>
2024-04-22 22:10:00 -0500

    listado de vehiculos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VehiculoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/vehiculos', [VehiculoController::class, 'index'])->name('vehiculos.index');
+Route::post('/vehiculos', [VehiculoController::class, 'store'])->name('vehiculos.store');
+Route::get('/vehiculos/create', [VehiculoController::class, 'create'])->name('vehiculos.create');
+Route::delete('/vehiculos/{vehiculo}', [VehiculoController::class, 'destroy'])->name('vehiculos.destroy');
+Route::put('/vehiculos/{vehiculo}', [VehiculoController::class, 'update'])->name('vehiculos.update');
+Route::get('/vehiculos/{vehiculo}/edit', [VehiculoController::class, 'edit'])->name('vehiculos.edit');
